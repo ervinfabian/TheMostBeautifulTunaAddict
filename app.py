c61f186ca2d8229ece6461e374b4fefe1e584975
ervinfabian <ervinfab@gmail.com>
2025-02-14 22:21:50 +0200

    seventh commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 
-st.title("A vilag legszebb tonhal fuggojenek weboldala!!!")
+st.title("A világ legszebb tonhal függőjenek !!!")
 
 # image = cv2.imread("reka.jpeg")
 
@@ -16,11 +16,10 @@ st.title("A vilag legszebb tonhal fuggojenek weboldala!!!")
 # Now do something with the image! For example, let's display it:
 st.image("reka.jpeg")
 
-# st.write("Kerjuk adja meg a heten elfogyasztasra szant tonhal mennyiseget mg-ban!")
 
-amount_tuna = st.number_input("Kerjuk adja meg a heten elfogyasztasra szant tonhal mennyiseget g-ban!")
+amount_tuna = st.number_input("Kérjük adja meg a héten elfogyasztásra szánt tonhal mennyiségét grammban!")
 
-bodyweight = st.number_input("Kerjuk adja meg testomeget kg-ban!")
+bodyweight = st.number_input("Kérjük adja meg a testtömegét kg-ban!")
 
 healthy_amount_of_mercury_for_Reka = bodyweight * 0.1
 
@@ -28,6 +27,6 @@ amount_of_mercury_in_1g_tuna = 1.2
 
 if (amount_tuna != 0) and (bodyweight != 0):
     if (amount_tuna * amount_of_mercury_in_1g_tuna) < healthy_amount_of_mercury_for_Reka * 7:
-        st.write("Batran elfogyaszthatja az osszes tervezett tonhalat!")
+        st.write("Bátran elfogyaszthatja az összes tervezett tonhal mennyiséget!")
     else:
-        st.write("Kerjuk fogyasszon lehetoleg kevesebb tonhalat a heten, hogy tovabb gyonyorkodhessunk Onben!")
+        st.write("Kérjük fogyasszon lehetőleg kevesebb tonhalat a héten, hogy tövább gyönyörködhessünk Önben!")
