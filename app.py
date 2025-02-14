e0476d47dddf179de60fd6d67acee7375c8ce150
ervinfabian <ervinfab@gmail.com>
2025-02-14 14:04:18 +0200

    third commit

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import streamlit as st
-
+import cv2
+import numpy as np
 
 st.title("A vilag legszebb tonhal fuggojenek weboldala!!!")
+
+image = cv2.imread("reka.jpeg")
+
+
+file_bytes = np.asarray(image, dtype=np.uint8)
+# opencv_image = cv2.imdecode(file_bytes, 1)
+
+# Now do something with the image! For example, let's display it:
+st.image(file_bytes)
+
+# st.write("Kerjuk adja meg a heten elfogyasztasra szant tonhal mennyiseget mg-ban!")
+
+amount_tuna = st.number_input("Kerjuk adja meg a heten elfogyasztasra szant tonhal mennyiseget mg-ban!")
+
+bodyweight = st.number_input("Kerjuk adja meg testomeget kg-ban!")
+
+healthy_amount_of_mercury_for_Reka = bodyweight * 0.1
+
+amount_of_mercury_in_1mg_tuna = 0.012
+
+if (amount_tuna != 0) and (bodyweight != 0):
+    if (amount_tuna * amount_of_mercury_in_1mg_tuna) < healthy_amount_of_mercury_for_Reka:
+        st.write("Batran elfogyaszthatja az osszes tervezett tonhalat!")
+    else:
+        st.write("Kerjuk fogyasszon lehetoleg kevesebb tonhalat a heten, hogy tovabb gyonyorkodhessunk Onben!")
 
 
 
